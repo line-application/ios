cd0f4dd62be10357336cc32014a90d3e72fa66f4
FelipeOliveira <Felipe.Oliveira01@edu.pucrs.br>
2021-05-04 16:14:55 -0300

    Initial Commit

---
+++ On line/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  On line
+//
+//  Created by Felipe Grosze Nipper de Oliveira on 04/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ On line/On_lineApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_lineApp.swift
+//  On line
+//
+//  Created by Felipe Grosze Nipper de Oliveira on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct On_lineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
