61560d9c4615d12f322c8ed4ed41179a2a018a12
jgabrielbiazus <j.gquevedo50@gmail.com>
2021-05-06 14:57:30 -0300

    Model ( LinePlace )

---
+++ On line/Model/LinePlace.swift
@@ -0,0 +1,26 @@
+//
+//  LinePlace.swift
+//  On line
+//
+//  Created by João Gabriel Biazus de Quevedo on 06/05/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct LinePlace {
+    
+    let enter_line: Date
+    let exit_line: Date
+    let called: Date
+    let invoked: Bool
+    let sucess: Bool
+    let peopleInLine: Int
+    let businessEmail: String
+    let numberOfPeople: Int
+    
+}
+
+func exitLine(){
+    
+}
