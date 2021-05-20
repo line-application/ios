f00242ee4da600618ed07f6771e73b92108d0725
Diego Henrique Silva Oliveira <53708053+DiegoHSO@users.noreply.github.com>
2021-05-20 15:35:04 -0300

    tk61 - hotfix: when account is confirmed asks user to log in again (correctly). also made some UI fixes.

--- On line/states/SettingsState.swift
+++ On line/states/SettingsState.swift
@@ -10,8 +10,6 @@ import Foundation
 class SettingsState: ObservableObject {
     @Published var isAuthenticated: Bool = false
     @Published var userType: UserType = UserType.CLIENT
-    @Published var needsConfirmation: Bool = false
-    @Published var clientId: String = ""
     @Published var isLoading: Bool = false
     @Published var showAlert: Bool = false
 }
